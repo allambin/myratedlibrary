83716876e0f6032391a74f2c25b82d029c80590e
allambin <annelise.lambin@gmail.com>
2017-03-03 14:46:22 +0100

    replace ArrayAdapter with CursorAdapter and display list of books from DB

---
+++ app/src/main/java/com/inextends/myratedlibrary/BookCursorAdapter.java
@@ -0,0 +1,33 @@
+package com.inextends.myratedlibrary;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.inextends.myratedlibrary.data.BookContract;
+
+public class BookCursorAdapter extends CursorAdapter {
+
+    public BookCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.book_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView titleTextView = (TextView) view.findViewById(R.id.text_title);
+        String title = cursor.getString(cursor.getColumnIndexOrThrow(BookContract.BookEntry.COLUMN_TITLE));
+
+        if (titleTextView != null) {
+            titleTextView.setText(title);
+        }
+    }
+}
--- app/src/main/java/com/inextends/myratedlibrary/MainActivity.java
+++ app/src/main/java/com/inextends/myratedlibrary/MainActivity.java
@@ -1,15 +1,24 @@
 package com.inextends.myratedlibrary;
 
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
+import android.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
+import com.inextends.myratedlibrary.data.BookContract;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private BookCursorAdapter mBookCursorAdapter;
+    private static final int LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
         books.add(new Book("Dome", "Stephen King", 5));
         books.add(new Book("Doctor Sleep", "Stephen King", 3));
 
-        BookAdapter bookAdapter = new BookAdapter(this, books);
-
         ListView booksListView = (ListView) findViewById(R.id.list_books);
-        booksListView.setAdapter(bookAdapter);
+
+        mBookCursorAdapter = new BookCursorAdapter(this, null);
+        booksListView.setAdapter(mBookCursorAdapter);
+
+        getLoaderManager().initLoader(LOADER_ID, null, this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +45,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public Loader onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                BookContract.BookEntry._ID,
+                BookContract.BookEntry.COLUMN_TITLE
+        };
+        return new CursorLoader(this, BookContract.BookEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mBookCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+        mBookCursorAdapter.swapCursor(null);
     }
 }
